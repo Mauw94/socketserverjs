7f4b91fdd98602e1574ff6bd7d6f8f0dabef84ca
Maurits <mauritsseelen@hotmail.com>
2022-12-13 15:49:46 +0100

    updated to correct socketio package

---
+++ controllers/roomController.ts
@@ -0,0 +1,41 @@
+import { Server, Socket } from 'socket.io'
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnMessage,
+  SocketController,
+  SocketIO
+} from 'socket-controllers'
+
+@SocketController()
+export class RoomController {
+
+  /**
+   * Listen to join_room event
+   * @param socket 
+   * @param io 
+   * @param message 
+   */
+  @OnMessage('join_room')
+  public async joinRoom(
+    @ConnectedSocket() socket: Socket,
+    @SocketIO() io: Server,
+    @MessageBody() message: any
+  ) {
+    const connectedSockets = io.sockets.adapter.rooms.get(message.roomId)
+    const currentConnectedroom = Array.from(socket.rooms.values()).filter(r => r === socket.id)
+
+    // check if room we're trying to connect to is full
+    if (connectedSockets && connectedSockets.size == 5) {
+      socket.emit('room_join_error', {
+        error: 'room is full'
+      })
+    }
+
+    // leave current room
+    if (currentConnectedroom) socket.leave(currentConnectedroom[0]);
+
+    // join room
+    await socket.join(message.roomId)
+  }
+}
